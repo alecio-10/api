08bfcd5e70bcf3337e6e6e5e50d60180151de8d6
alecio viola payares <alecio1052@gmail.com>
2023-03-01 18:40:22 -0500

    api

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json","-w");
 const port = process.env.PORT ||3001;
 const middleware = jsonServer.defaults();
 const cors = require("cors");
 server.use(cors());
-server.use(jsonServer.bodyParser)
 server.use(router);
 server.use(middleware);
 server.listen(port)
